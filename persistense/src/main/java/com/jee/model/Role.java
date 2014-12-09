3432f03228387d9b21680e73fe274e414cef92f1
Aliaksandr Belavusau <abelavusau@gmail.com>
2014-12-09 11:18:32 -0300

    fixed formatting

--- persistense/src/main/java/com/jee/model/Role.java
+++ persistense/src/main/java/com/jee/model/Role.java
@@ -4,52 +4,48 @@ import java.io.Serializable;
 import javax.persistence.*;
 import java.util.Set;
 
-
 /**
  * The persistent class for the roles database table.
  * 
  */
 @Entity
-@Table(name="roles")
-@NamedQuery(name="Role.findAll", query="SELECT r FROM Role r")
+@Table(name = "roles")
+@NamedQuery(name = "Role.findAll", query = "SELECT r FROM Role r")
 public class Role implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private int id;
-	private String role;
-	private Set<User> users;
+    private static final long serialVersionUID = 1L;
+    private int               id;
+    private String            role;
+    private Set<User>         users;
 
-	public Role() {
-	}
+    public Role() {
+    }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return this.id;
+    }
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	public int getId() {
-		return this.id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public String getRole() {
+        return this.role;
+    }
 
+    public void setRole(String role) {
+        this.role = role;
+    }
 
-	public String getRole() {
-		return this.role;
-	}
+    // bi-directional many-to-many association to User
+    @ManyToMany(mappedBy = "roles")
+    public Set<User> getUsers() {
+        return this.users;
+    }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-
-	//bi-directional many-to-many association to User
-	@ManyToMany(mappedBy="roles")
-	public Set<User> getUsers() {
-		return this.users;
-	}
-
-	public void setUsers(Set<User> users) {
-		this.users = users;
-	}
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 
 }
