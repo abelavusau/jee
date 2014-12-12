9029a1661edacf4a5921d0d05ae879a56a1209b8
Aliaksandr_Belavusau <Aliaksandr_Belavusau@epam.com>
2014-12-12 10:42:18 -0300

    updated db script

--- persistense/src/main/resources/db.sql
+++ persistense/src/main/resources/db.sql
@@ -9,13 +9,13 @@
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- Dumping data for table platform.roles: ~2 rows (approximately)
-DELETE FROM `roles`;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` (`id`, `role`) VALUES
-	(1, 'ROLE_ADMIN'),
-	(2, 'ROLE_USER');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+-- Dumping data for table platform.groups: ~2 rows (approximately)
+DELETE FROM `groups`;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` (`id`, `name`, `desc`) VALUES
+	(1, 'USER', 'Regular users'),
+	(2, 'ADMIN', 'Administration users');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 
 -- Dumping data for table platform.users: ~0 rows (approximately)
 DELETE FROM `users`;
@@ -24,12 +24,10 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `ena
 	(1, 'Aliaksandr', 'Belavusau', 'abelavusau', '$2a$10$hv2kxATuhK78HPz9eVMXEuRUQvg0YBA9Z/QqqidprBNQkqkaj4tFG', 1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
--- Dumping data for table platform.user_role: ~0 rows (approximately)
-DELETE FROM `user_role`;
-/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` (`id`, `user_id`, `role_id`) VALUES
-	(1, 1, 2);
-/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+-- Dumping data for table platform.users_groups: ~0 rows (approximately)
+DELETE FROM `users_groups`;
+/*!40000 ALTER TABLE `users_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_groups` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
